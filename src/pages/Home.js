004d9f054d16d908a9e803edfa43a77073be3db6
Imran Hossen <freelancerimran81@gmail.com>
2022-05-19 16:10:35 +0600

    updated

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='bg-accent'>
+            <div className='mid-content'>
+                <h1>This is home page</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
